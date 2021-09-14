8a2e505a49383ae3bb96a52ab011a282fc3071ab
joseph.sang <>
2021-09-14 22:45:59 +0900

    #로딩중 #검색결과없음 #구조 변경

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,19 +1,18 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { SearchBar, MovieItem } from '../components';
-import styled from 'styled-components';
 
 const Search = () => {
 
   const [movies, setMovies] = useState([]);
   const [keyword, setKeyword] = useState('');
-  const [loading, setLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [isResultEmpty, setIsResultEmpty] = useState(false)
   
   const getMovie = async (ss) => {
     const ID_KEY = 'sA6DKWhkyY6B1sPt9ykg';
     const SECRET_KEY = 'jIeE2IpabY';
     const search = ss;
-    const content = document.querySelector('.cont_movie');
 
     const data = {
       params: {
@@ -27,16 +26,21 @@ const Search = () => {
     }
     
     try{
-      setLoading(true);
+      setIsLoading(true);
       const response = await axios.get('/v1/search/movie.json', data)
       const res = response.data.items;
-      setMovies(res);
-      setLoading(false);
+      setIsLoading(false);
+      if(res.length === 0){
+        setIsResultEmpty(true);
+      }else{
+        setMovies(res);
+        setIsResultEmpty(false);
+      }
     }catch(e){
       alert(e)
     }
   }
-    
+
   const onHandleChange = (ee) => {
     setKeyword(ee);
   }
@@ -45,7 +49,6 @@ const Search = () => {
     getMovie(ee);
   }
 
-  console.log(movies.length);
 
   return (
     <article className="content-article">
@@ -53,26 +56,15 @@ const Search = () => {
         <SearchBar placeHolder="영화 제목을 검색하세요" onUserInput={onHandleChange} onUserSubmit={onHandleSubmit} keyword={keyword} />
       </div>
       <div className="cont_movie">
-        {loading 
-          ? <Loading>Loading...</Loading>
-          : movies.map((movie,idx) => 
-            <MovieItem 
-                key={idx} 
-                title={movie.title} subtitle={movie.subtitle} userRating={movie.userRating} 
-                link={movie.link} pubDate={movie.pubDate} director={movie.director}
-                image={movie.image}
-            /> 
-            )
+        {
+          isResultEmpty 
+          ? <p>검색결과가 없습니다.</p> 
+          : <MovieItem movies={movies} isLoading={isLoading} />
         }
-        </div>
+      </div>
     </article>
   )
 }
 
-const Loading = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100vh;
-`
 
 export default Search
